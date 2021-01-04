8073abe1cdb7b574074674b4af1c28de129b56df
Vhaustro <mandenno228@gmail.com>
2021-01-04 14:59:20 +0300

    Strings in Python

---
+++ Python 2 Strings.py
@@ -0,0 +1,24 @@
+first_name = 'Dennis'
+last_name = 'Micheal'
+print (first_name + last_name)
+
+sentence = 'The dog is on a leaf'
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.lower())
+print(sentence.capitalize())
+print(sentence.count('a'))
+
+
+try:
+    pass
+except expression as identifier: def initial(self, parameter_list):
+    """
+    docstring
+    """
+    pass
+    pass
+else:
+    pass
+finally:
+    pass
